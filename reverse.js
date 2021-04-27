0524cf71f4604d45efca105105f0d8d7472596e2
SophiaL1024 <sophiafengliu@gmail.com>
2021-04-27 02:56:44 +0000

    use template literal to reverse a string

--- reverse.js
+++ reverse.js
@@ -2,7 +2,7 @@ const args = process.argv;
 let newStr = '';
 const reverse = function(str) {
   for (let i = str[2].length - 1; i >= 0; i--) {
-    newStr = newStr + str[2][i];
+    newStr = `${newStr}${str[2][i]}`;
   }
   console.log(newStr);
 };
